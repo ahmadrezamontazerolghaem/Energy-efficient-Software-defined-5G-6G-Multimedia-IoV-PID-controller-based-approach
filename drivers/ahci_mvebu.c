696293d404a27aab8c8750a8a5af13eb0134da6c
ahmadrezamontazerolghaem <ahmadreza.20012@gmail.com>
2021-07-24 13:00:13 +0430

    Update ahci_mvebu.c

--- drivers/ahci_mvebu.c
+++ drivers/ahci_mvebu.c
@@ -6,7 +6,6 @@
 #include <linux/platform_device.h>
 #include "ahcit.h"
 
-
 #define AHCI_VENDOR_SPECIFIC_0_ADDR  0xa0
 #define AHCI_VENDOR_SPECIFIC_0_DATA  0xa4
 #define AHCI_WINDOW_CTRL(win)	(0x60 + ((win) << 4))
